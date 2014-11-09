34f1376b25b8879c7cbb1d5480a10d21ccd72fe2
vsrv.zenkr <zenkr@qq.com>
2014-11-09 00:12:43 -0800

    support lv.1 nav

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -19,10 +19,15 @@ class SiteController extends BaseController {
 
 	public function withPattern($clubname,$str)
 	{
-        if (View::exists($str))
-        {
-            return $str;
+        if (file_exists("./sites/".$clubname."/conf.json")){
+            $conf_file = "./sites/".$clubname."/conf.json";
+            $conf_str = file_get_contents($conf_file);
+            $conf = json_decode($conf_str, true);
         }
-		return Response::view('errors.404', array(), 404);
+        if (View::exists('sites.'.$clubname.'.'.$str))
+        {
+            return View::make('sites.'.$clubname.'.'.$str, $conf);
+        }
+        return Response::view('errors.404', array(), 404);
 	}
 }
