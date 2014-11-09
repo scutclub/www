84cb32827cc1a30cfbcc324e941c8f4b491044f5
vsrv.zenkr <zenkr@qq.com>
2014-11-08 21:05:44 -0800

    add subdomain page config file & view

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -4,7 +4,17 @@ class SiteController extends BaseController {
 
 	public function homePage($clubname)
 	{
-		return $clubname;
+        if (file_exists("./sites/".$clubname."/conf.json")){
+            $conf_file = "./sites/".$clubname."/conf.json";
+            $conf_str = file_get_contents($conf_file);
+            $conf = json_decode($conf_str, true);
+        }
+        if (View::exists('sites.'.$clubname.'.index'))
+        {
+            return View::make('sites.'.$clubname.'.index', $conf);
+        }
+        return Response::view('errors.404', array(), 404);
+
 	}
 
 	public function withPattern($clubname,$str)
